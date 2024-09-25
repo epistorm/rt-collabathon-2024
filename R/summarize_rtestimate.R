96650b979718a89e0d1ab1578712d446767da335
George G. Vega Yon <g.vegayon@gmail.com>
2024-09-25 09:55:45 -0600

    Adding new method and constructor for summrt_summary object

--- R/summarize_rtestimate.R
+++ R/summarize_rtestimate.R
@@ -1,3 +1,43 @@
+
+#' Create a new summary object
+#' @param date Integer vector. vector of index dates.
+#' @param median Double vector. vector of median values.
+#' @param lb Double vector. vector of lower bounds.
+#' @param up Double vector. vector of upper bounds.
+#' @param package String. Name of the package.
+#' @export 
+new_summarize <- function(
+  date, median, lb, up, package
+) {
+
+  # Asserting the types
+  checkmate::assert_integer(date)
+  checkmate::assert_double(median)
+  checkmate::assert_double(lb)
+  checkmate::assert_double(up)
+  checkmate::assert_string(package)
+
+  # Checking the length
+  len_date <- length(date)
+  len_median <- length(median)
+  len_lb <- length(lb)
+  len_up <- length(up)
+  if (len_date != len_median || len_date != len_lb || len_date != len_up) {
+    stop("The length of the date, median, lb, and up should be the same.")
+  }
+
+  structure(
+    list(
+      date = date,
+      median = median,
+      lb = lb,
+      up = up,
+      package = package
+    ),
+    class = "summrt_summary"
+  )
+}
+
 #' Extract Rt estimation from a model fit
 #' @param x Object to extract Rt from.
 #' @param ... Additional arguments passed to methods.
@@ -69,4 +109,21 @@ summarize_rtestimate.epinow <- function(x, level = 0.95, ...) {
   checkmate::assert_number(level, lower = 0, upper = 1)
 
   # res <- x$estimates$summarized |> dplyr::select()
+}
+
+#' @export 
+#' @rdname summarize_rtestimate
+summarize_rtestimate.estimate_R <- function(x, ...) {
+  if (!requireNamespace("EpiEstim", quietly = TRUE)) {
+    cli::cli_abort("You must install the {.pkg EpiEstim} package for this functionality.")
+  }
+  checkmate::assert_number(level, lower = 0, upper = 1)
+  
+  new_summarize(
+    date = x$R$t_end,
+    median = x$R$`Median(R)`,
+    lb = x$R$`Quantile.0.025(R)`,
+    up = x$R$`Quantile.0.975(R)`,
+    package = "EpiEstim"
+  )
 }
